be6041db342173234873fb392829aec64202d8cc
Jofre Faraudo <37667469+JofreFaraudo@users.noreply.github.com>
2020-10-27 11:28:18 +0100

    Update constants.cpp

--- constants.cpp
+++ constants.cpp
@@ -5,12 +5,14 @@
 #define sizex // screen size
 #define sizey // Screen size
 #define jthreshold 100 // Threshold for joystick
+#define speed // Speed
 
 // Vars
 int dir; // Dirs: 0 u, 1 r, 2 d, 3 l
 int x; // Current pos x
 int y; // current pos y
 int p; // Current points
+int mc; // Speed var auxiliar, counter
 int[sizex][sizey] apple;
 int[] snake; // list array, pendent array dinamic de touples coords
 
