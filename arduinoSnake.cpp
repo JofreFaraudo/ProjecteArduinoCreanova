fac3805197c9da6bb840c310617a55b885deeb8e
Jofre Faraudo <37667469+JofreFaraudo@users.noreply.github.com>
2020-10-27 11:20:04 +0100

    Update playing variable
    Pretty print, updated playing variable

--- arduinoSnake.cpp
+++ arduinoSnake.cpp
@@ -1,6 +1,8 @@
 void main(){
-	if(digitalRead(pb))
-		return;
+	if(digitalRead(pb)){
+		playing = false;
+		return
+	}
 	if(analogRead(jx)>1023-jthreshold )
 		dir = 1;
 	if((analogRead(jy)>1023-jthreshold )
@@ -11,26 +13,27 @@ void main(){
 		dir = 2;
 	switch(dir){
 		case 0:
-		y++;
-		break;
+			y++;
+			break;
 		case 1:
-		x++;
-		break;
+			x++;
+			break;
 		case 2:
-		y--;
-		break;
+			y--;
+			break;
 		case 3:
-		x--;
-		break;
+			x--;
+			break;
 	}
-	   if(mc = 0){
-	   mc = speed;
-		   moveSnake();
-	   }
-	   else mc--;3
+	if(mc = 0){
+		mc = speed;
+		moveSnake();
+	}
+	else
+	   mc--;3
 	if(apple[x][y]){
 		p++;
 		apple[x][y] = false;
 		apple[random[sizex]][random[sizey]] = true;
 	}
-	}
+}
