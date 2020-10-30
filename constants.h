f8a22bfdf472d185cd9f969f79e6d99bee8bc0dd
Jofre Faraudo <37667469+JofreFaraudo@users.noreply.github.com>
2020-10-30 12:11:15 +0100

    Update constants.h

--- constants.h
+++ constants.h
@@ -2,10 +2,12 @@
 #define pb // pushbutton pin
 #define jx // Joystic x
 #define jy // Joystick y
-#define sizex 30 // screen size
-#define sizey 60 // Screen size
+#define sizex 30 // screen size x
+#define sizey 60 // Screen size y
 #define jthreshold 100 // Threshold for joystick
 #define speed 1000 // Speed
+#define initialTailSize 3 // The size of the tail when beginning the game
+#define initialApples 2 // Apples when beginning the game
 
 // Global vars
 int dir; // Dirs: 0 u, 1 r, 2 d, 3 l
@@ -23,3 +25,5 @@ void newApple();
 void newApple(int q);
 void gameScreen();
 void titleScreen();
+void endGame();
+void initGame();
