6831a2684d02b044889acfda1b82a1780548bf89
Jofre Faraudo <37667469+JofreFaraudo@users.noreply.github.com>
2020-11-02 21:14:11 +0100

    Update screen.h

--- screen.h
+++ screen.h
@@ -1,5 +1,5 @@
 void titleScreen(){
-  Serial.println("Benvolgut al Snake del ousmane i al Snake d\'en Viviano Espero que gaudeixis del teu Snake, merit de Juffrrenfa, pero idea de\'n Vivano");
+  Serial.println("Benvolgut al Snake de l\'ousmane i al Snake d\'en Viviano Espero que gaudeixis del teu Snake, merit de Juffrrenfa, pero idea de\'n Vivano");
 }
 void gameScreen(){
   Serial.println("Tu no jugaras mai, jaja es broma xddd");
