3d814df460765294e2d802e79ef756213c51c91f
Viviano112 <73524732+Viviano112@users.noreply.github.com>
2020-11-02 11:32:34 +0100

    Update screen.h

--- screen.h
+++ screen.h
@@ -1,6 +1,7 @@
 void titleScreen(){
-  return;
+  Serial.println("Benvolgut al Snake del ousmane i al Snake d\'en Viviano Espero que gaudeixis del teu Snake, merit de Juffrrenfa, pero idea de\'n Vivano");
 }
 void gameScreen(){
-  return;
+  Serial.println("Tu no jugaras mai, jaja es broma xddd");
+  Serial.println(string(playing)+' '+ string(x)+' '+ string(y)+' '+ string(mc)+' '+ string(p));
 }
