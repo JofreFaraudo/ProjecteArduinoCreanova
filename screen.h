c7bbb5408c14de9db00a18b65a92ab0028cbfcc0
Jofre Faraudo <37667469+JofreFaraudo@users.noreply.github.com>
2020-11-02 21:13:08 +0100

    Update screen.h

--- screen.h
+++ screen.h
@@ -4,6 +4,9 @@ void titleScreen(){
 void gameScreen(){
   Serial.println("Tu no jugaras mai, jaja es broma xddd");
   Serial.println("playing: "+string(playing)+", x: "+ string(x)+", y: "+ string(y)+", mc: "+ string(mc)+", pts: "+ string(p)+", apples:");
-  //for(int i = 0; i < sizex*sizey; i++)
-    
+  for(int i = 0; i < sizex*sizey; i++)
+    Serial.print(i+':'+apples[i]+' ');
+  Serial.println("tail");
+  for(int i = 0; i < snakeTail.Count(); i++)
+    Serial.print(i+':'+snakeTail[i]+' ');
 }
